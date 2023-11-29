89404aea741cdaa3bc76d29c20d135e4db627152
Davi Moura <davisilvag4mer@gmail.com>
2023-11-29 11:56:39 -0300

    feat: basic shortlinks

---
+++ l/index.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT || 8080
+
+app.get('/', (req, res) => {
+  res.redirect('https://links.davimoura.dev')
+})
+
+app.get('/:name', async (req, res) => {
+  const { name } = req.params
+  const { data } = await axios.get(`https://api.ordo.davimoura.dev/shortlinks/${name}`)
+  const link = data?.url
+  if (!link) return res.redirect('https://links.davimoura.dev')
+  res.redirect(link)
+})
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
